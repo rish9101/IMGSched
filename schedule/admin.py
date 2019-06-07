7aa7c32e8fddba2be517030ce49beda6d6de996d
rish9101 <jack_0f_spades@localhost.localdomain>
2019-06-07 23:08:29 +0530

    Add models, serializers and API endpoint views
    Currently the app uses django default login and a custom user model.
    All API endpoint views have been tested

--- schedule/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import User, Users, MeetingsTable, CommentsTable
-# Register your models here.
-
-admin.site.register(User)
-admin.site.register(Users)
-admin.site.register(MeetingsTable)
-admin.site.register(CommentsTable)
--- schedule/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-        path('',views.schedule_list, name ='home'),
-    ]
